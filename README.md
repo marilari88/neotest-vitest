b0cb69fe46a4fd4e975fb7c6920eb1b14e5c9cd6
Marco Ilari <marilari88@gmail.com>
2022-08-20 17:16:20 +0200

    🎉 Start from neotest-jest

---
+++ README.md
@@ -0,0 +1,53 @@
+# neotest-jest
+
+[![build](https://github.com/haydenmeade/neotest-jest/actions/workflows/workflow.yaml/badge.svg)](https://github.com/haydenmeade/neotest-jest/actions/workflows/workflow.yaml)
+
+This plugin provides a jest adapter for the [Neotest](https://github.com/rcarriga/neotest) framework.
+**It is currently a work in progress**. It will be transferred to the official neotest organisation (once it's been created).
+
+## Installation
+
+Using packer:
+
+```lua
+use({
+  'rcarriga/neotest',
+  requires = {
+    ...,
+    'haydenmeade/neotest-jest',
+  }
+  config = function()
+    require('neotest').setup({
+      ...,
+      adapters = {
+        require('neotest-jest')({
+          jestCommand = "npm test --",
+          jestConfigFile = "custom.jest.config.ts",
+          env = { CI = true },
+          cwd = function(path)
+            return vim.fn.getcwd()
+          end,
+        }),
+      }
+    })
+  end
+})
+```
+
+## Usage
+
+See neotest's documentation for more information on how to run tests.
+
+## :gift: Contributing
+
+Please raise a PR if you are interested in adding new functionality or fixing any bugs. When submitting a bug, please include an example spec that can be tested.
+
+To trigger the tests for the adapter, run:
+
+```sh
+./scripts/test
+```
+
+## Bug Reports
+
+Please file any bug reports and I _might_ take a look if time permits otherwise please submit a PR, this plugin is intended to be by the community for the community.
---
+++ lua/neotest-jest/init.lua
@@ -0,0 +1,362 @@
+---@diagnostic disable: undefined-field
+local async = require("neotest.async")
+local lib = require("neotest.lib")
+local logger = require("neotest.logging")
+local util = require("neotest-jest.util")
+
+---@class neotest.JestOptions
+---@field jestCommand? string|fun(): string
+---@field jestConfigFile? string|fun(): string
+---@field env? table<string, string>|fun(): table<string, string>
+---@field cwd? string|fun(): string
+
+---@type neotest.Adapter
+local adapter = { name = "neotest-jest" }
+
+adapter.root = lib.files.match_root_pattern("package.json")
+
+---@param file_path? string
+---@return boolean
+function adapter.is_test_file(file_path)
+  if file_path == nil then
+    return false
+  end
+
+  if string.match(file_path, "__tests__") then
+    return true
+  end
+
+  for _, x in ipairs({ "spec", "test" }) do
+    for _, ext in ipairs({ "js", "jsx", "coffee", "ts", "tsx" }) do
+      if string.match(file_path, x .. "%." .. ext .. "$") then
+        return true
+      end
+    end
+  end
+
+  return false
+end
+
+---@async
+---@return neotest.Tree | nil
+function adapter.discover_positions(path)
+  local query = [[
+    ; -- Namespaces --
+    ; Matches: `describe('context')`
+    ((call_expression
+      function: (identifier) @func_name (#eq? @func_name "describe")
+      arguments: (arguments (string (string_fragment) @namespace.name) (arrow_function))
+    )) @namespace.definition
+    ; Matches: `describe.only('context')`
+    ((call_expression
+      function: (member_expression
+        object: (identifier) @func_name (#any-of? @func_name "describe")
+      )
+      arguments: (arguments (string (string_fragment) @namespace.name) (arrow_function))
+    )) @namespace.definition
+    ; Matches: `describe.each(['data'])('context')`
+    ((call_expression
+      function: (call_expression
+        function: (member_expression
+          object: (identifier) @func_name (#any-of? @func_name "describe")
+        )
+      )
+      arguments: (arguments (string (string_fragment) @namespace.name) (arrow_function))
+    )) @namespace.definition
+
+    ; -- Tests --
+    ; Matches: `test('test') / it('test')`
+    ((call_expression
+      function: (identifier) @func_name (#any-of? @func_name "it" "test")
+      arguments: (arguments (string (string_fragment) @test.name) (arrow_function))
+    )) @test.definition
+    ; Matches: `test.only('test') / it.only('test')`
+    ((call_expression
+      function: (member_expression
+        object: (identifier) @func_name (#any-of? @func_name "test" "it")
+      )
+      arguments: (arguments (string (string_fragment) @test.name) (arrow_function))
+    )) @test.definition
+    ; Matches: `test.each(['data'])('test') / it.each(['data'])('test')`
+    ((call_expression
+      function: (call_expression
+        function: (member_expression
+          object: (identifier) @func_name (#any-of? @func_name "it" "test")
+        )
+      )
+      arguments: (arguments (string (string_fragment) @test.name) (arrow_function))
+    )) @test.definition
+  ]]
+
+  return lib.treesitter.parse_positions(path, query, { nested_tests = true })
+end
+
+---@param path string
+---@return string
+local function getJestCommand(path)
+  local rootPath = util.find_node_modules_ancestor(path)
+  local jestBinary = util.path.join(rootPath, "node_modules", ".bin", "jest")
+
+  if util.path.exists(jestBinary) then
+    return jestBinary
+  end
+
+  return "jest"
+end
+
+local jestConfigPattern = util.root_pattern("jest.config.{js,ts}")
+
+---@param path string
+---@return string|nil
+local function getJestConfig(path)
+  local rootPath = jestConfigPattern(path)
+
+  if not rootPath then
+    return nil
+  end
+
+  local jestJs = util.path.join(rootPath, "jest.config.js")
+  local jestTs = util.path.join(rootPath, "jest.config.ts")
+
+  if util.path.exists(jestTs) then
+    return jestTs
+  end
+
+  return jestJs
+end
+
+local function escapeTestPattern(s)
+  return (
+    s
+      :gsub("%(", "%\\(")
+      :gsub("%)", "%\\)")
+      :gsub("%]", "%\\]")
+      :gsub("%[", "%\\[")
+      :gsub("%*", "%\\*")
+      :gsub("%+", "%\\+")
+      :gsub("%-", "%\\-")
+      :gsub("%?", "%\\?")
+      :gsub("%$", "%\\$")
+      :gsub("%^", "%\\^")
+      :gsub("%/", "%\\/")
+  )
+end
+
+local function get_strategy_config(strategy, command)
+  local config = {
+    dap = function()
+      return {
+        name = "Debug Jest Tests",
+        type = "pwa-node",
+        request = "launch",
+        args = { unpack(command, 2) },
+        runtimeExecutable = command[1],
+        console = "integratedTerminal",
+        internalConsoleOptions = "neverOpen",
+      }
+    end,
+  }
+  if config[strategy] then
+    return config[strategy]()
+  end
+end
+
+local function getEnv(specEnv)
+  return specEnv
+end
+
+---@param path string
+---@return string|nil
+local function getCwd(path)
+  return nil
+end
+
+---@param args neotest.RunArgs
+---@return neotest.RunSpec | nil
+function adapter.build_spec(args)
+  local results_path = async.fn.tempname() .. ".json"
+  local tree = args.tree
+
+  if not tree then
+    return
+  end
+
+  local pos = args.tree:data()
+  local testNamePattern = "'.*'"
+
+  if pos.type == "test" then
+    testNamePattern = "'" .. escapeTestPattern(pos.name) .. "$'"
+  end
+
+  if pos.type == "namespace" then
+    testNamePattern = "'^" .. escapeTestPattern(pos.name) .. "'"
+  end
+
+  local binary = getJestCommand(pos.path)
+  local config = getJestConfig(pos.path) or "jest.config.js"
+  local command = vim.split(binary, "%s+")
+  if util.path.exists(config) then
+    -- only use config if available
+    table.insert(command, "--config=" .. config)
+  end
+
+  vim.list_extend(command, {
+    "--no-coverage",
+    "--testLocationInResults",
+    "--verbose",
+    "--json",
+    "--outputFile=" .. results_path,
+    "--testNamePattern=" .. testNamePattern,
+    pos.path,
+  })
+
+  return {
+    command = command,
+    cwd = getCwd(pos.path),
+    context = {
+      results_path = results_path,
+      file = pos.path,
+    },
+    strategy = get_strategy_config(args.strategy, command),
+    env = getEnv(args[2] and args[2].env or {}),
+  }
+end
+
+local function cleanAnsi(s)
+  return s:gsub("\x1b%[%d+;%d+;%d+;%d+;%d+m", "")
+    :gsub("\x1b%[%d+;%d+;%d+;%d+m", "")
+    :gsub("\x1b%[%d+;%d+;%d+m", "")
+    :gsub("\x1b%[%d+;%d+m", "")
+    :gsub("\x1b%[%d+m", "")
+end
+
+local function findErrorPosition(file, errStr)
+  -- Look for: /path/to/file.js:123:987
+  local regexp = file:gsub("([^%w])", "%%%1") .. "%:(%d+)%:(%d+)"
+  local _, _, errLine, errColumn = string.find(errStr, regexp)
+
+  return errLine, errColumn
+end
+
+local function parsed_json_to_results(data, output_file, consoleOut)
+  local tests = {}
+
+  for _, testResult in pairs(data.testResults) do
+    local testFn = testResult.name
+
+    for _, assertionResult in pairs(testResult.assertionResults) do
+      local status, name = assertionResult.status, assertionResult.title
+
+      if name == nil then
+        logger.error("Failed to find parsed test result ", assertionResult)
+        return {}
+      end
+
+      local keyid = testFn
+
+      for _, value in ipairs(assertionResult.ancestorTitles) do
+        keyid = keyid .. "::" .. value
+      end
+
+      keyid = keyid .. "::" .. name
+
+      if status == "pending" then
+        status = "skipped"
+      end
+
+      tests[keyid] = {
+        status = status,
+        short = name .. ": " .. status,
+        output = consoleOut,
+        location = assertionResult.location,
+      }
+
+      if not vim.tbl_isempty(assertionResult.failureMessages) then
+        local errors = {}
+
+        for i, failMessage in ipairs(assertionResult.failureMessages) do
+          local msg = cleanAnsi(failMessage)
+          local errorLine, errorColumn = findErrorPosition(testFn, msg)
+
+          errors[i] = {
+            line = (errorLine or assertionResult.location.line) - 1,
+            column = (errorColumn or 1) - 1,
+            message = msg,
+          }
+
+          tests[keyid].short = tests[keyid].short .. "\n" .. msg
+        end
+
+        tests[keyid].errors = errors
+      end
+    end
+  end
+
+  return tests
+end
+
+---@async
+---@param spec neotest.RunSpec
+---@return neotest.Result[]
+function adapter.results(spec, b, tree)
+  local output_file = spec.context.results_path
+
+  local success, data = pcall(lib.files.read, output_file)
+
+  if not success then
+    logger.error("No test output file found ", output_file)
+    return {}
+  end
+
+  local ok, parsed = pcall(vim.json.decode, data, { luanil = { object = true } })
+
+  if not ok then
+    logger.error("Failed to parse test output json ", output_file)
+    return {}
+  end
+
+  local results = parsed_json_to_results(parsed, output_file, b.output)
+
+  return results
+end
+
+local is_callable = function(obj)
+  return type(obj) == "function" or (type(obj) == "table" and obj.__call)
+end
+
+setmetatable(adapter, {
+  ---@param opts neotest.JestOptions
+  __call = function(_, opts)
+    if is_callable(opts.jestCommand) then
+      getJestCommand = opts.jestCommand
+    elseif opts.jestCommand then
+      getJestCommand = function()
+        return opts.jestCommand
+      end
+    end
+    if is_callable(opts.jestConfigFile) then
+      getJestConfig = opts.jestConfigFile
+    elseif opts.jestConfigFile then
+      getJestConfig = function()
+        return opts.jestConfigFile
+      end
+    end
+    if is_callable(opts.env) then
+      getEnv = opts.env
+    elseif opts.env then
+      getEnv = function(specEnv)
+        return vim.tbl_extend("force", opts.env, specEnv)
+      end
+    end
+    if is_callable(opts.cwd) then
+      getCwd = opts.cwd
+    elseif opts.cwd then
+      getCwd = function()
+        return opts.cwd
+      end
+    end
+    return adapter
+  end,
+})
+
+return adapter
