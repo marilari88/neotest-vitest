556886a272fd698bb4c2e59c431cae78dfe2ff2b
Marco Ilari <marilari88@gmail.com>
2022-08-21 02:40:59 +0200

    🐛 Fix wrong test and describe commands

--- lua/neotest-vitest/init.lua
+++ lua/neotest-vitest/init.lua
@@ -201,11 +201,11 @@ function adapter.build_spec(args)
   end
 
   vim.list_extend(command, {
-    "run",
+    "--run",
     "--reporter=verbose",
     "--reporter=json",
     "--outputFile=" .. results_path,
-    "'--testNamePattern=" .. testNamePattern .. "'",
+    "--testNamePattern=" .. testNamePattern .. "",
     pos.path,
   })
 
