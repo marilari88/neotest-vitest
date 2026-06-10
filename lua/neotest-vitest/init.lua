35cc5e2ccca68aa8b36ec3102ade807606a738c8
Ben Elan <no-reply@benelan.dev>
2026-06-10 11:56:26 -0700

    fix: watch flag could be passed to extra_args

--- lua/neotest-vitest/init.lua
+++ lua/neotest-vitest/init.lua
@@ -311,6 +311,16 @@ function adapter.build_spec(args)
     table.insert(command, "--config=" .. config)
   end
 
+  vim.list_extend(command, {
+    "--reporter=verbose",
+    "--reporter=json",
+    "--outputFile=" .. results_path,
+    "--testNamePattern=" .. testNamePattern,
+    vim.fs.normalize(pos.path),
+  })
+
+  vim.list_extend(command, args.extra_args or {})
+
   if
     not vim.list_contains(command, "-w")
     and not vim.list_contains(command, "--watch")
@@ -321,16 +331,6 @@ function adapter.build_spec(args)
       "--watch=false",
     })
   end
-
-  vim.list_extend(command, {
-    "--reporter=verbose",
-    "--reporter=json",
-    "--outputFile=" .. results_path,
-    "--testNamePattern=" .. testNamePattern,
-    vim.fs.normalize(pos.path),
-  })
-
-  vim.list_extend(command, args.extra_args or {})
 
   local cwd = getCwd(pos.path)
 
