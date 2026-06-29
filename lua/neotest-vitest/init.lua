ab5a73d6ec9d33e807d6c67ce5169c4ca4a5d7f3
Ben Elan <no-reply@benelan.dev>
2026-06-28 17:18:45 -0700

    fix: ensure watch flag is only passed once (#94)
    * fix: ensure watch flag is only passed once
    
    * chore: cleanup
    
    * fix: watch flag could be passed to extra_args
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Marco Ilari <marilari88@gmail.com>

--- lua/neotest-vitest/init.lua
+++ lua/neotest-vitest/init.lua
@@ -312,7 +312,6 @@ function adapter.build_spec(args)
   end
 
   vim.list_extend(command, {
-    "--watch=false",
     "--reporter=verbose",
     "--reporter=json",
     "--outputFile=" .. results_path,
@@ -321,6 +320,17 @@ function adapter.build_spec(args)
   })
 
   vim.list_extend(command, args.extra_args or {})
+
+  if
+    not vim.list_contains(command, "-w")
+    and not vim.list_contains(command, "--watch")
+    and not vim.list_contains(command, "--watch=true")
+    and not vim.list_contains(command, "--watch=false")
+  then
+    vim.list_extend(command, {
+      "--watch=false",
+    })
+  end
 
   local cwd = getCwd(pos.path)
 
